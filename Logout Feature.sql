62dcc1391d90054129e211fcf955038d8acfaca6
editionguy <ghyh28@gmail.com>
2025-04-17 19:34:46 +0800

    Update Logout Feature.sql

--- Logout Feature.sql
+++ Logout Feature.sql
@@ -1,9 +1,7 @@
--- Update LoginHistory table to set logoutTime
 UPDATE LoginHistory
 SET logoutTime = NOW()
 WHERE userID = 5 AND logoutTime IS NULL;
 
--- Expire the SessionToken by updating its expiryTime
 UPDATE SessionToken
 SET expiryTime = NOW()   -- Expiring the session immediately
 WHERE userID = 5 AND expiryTime > NOW();  -- Ensure only active sessions are updated
